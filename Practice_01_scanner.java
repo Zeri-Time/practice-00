ba4f8466242c31133413f34a2bbaca14a1c6ea4f
Zeri-Time <blee.940604@gmail.com>
2025-05-29 20:58:46 +0900

    practice_java

---
+++ Practice_01_scanner.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Practice_01_scanner {
+
+
+    public static void main(String[]args){
+
+        System.out.println("이름, 나이, 키 순서대로 입력");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+        // next(): 공백 이전까지의 문자열을 읽음
+        int age = scanner.nextInt();
+        // nextInt(): 공백 이전까지의 정수를 읽음
+        double height = scanner.nextDouble();
+//        boolean single = scanner.nextBoolean();
+        // nextLine: 엔터 이전까지의 문자열을 읽음
+
+
+        System.out.println("이름은 " + name + ", ");
+        //println: 줄바꿈
+        System.out.print("나이는 " + age + "살, ");
+        System.out.println("키는 " + height + "cm ");
+        
+
+        scanner.close();
+
+        System.out.println("완료");
+    }
+}

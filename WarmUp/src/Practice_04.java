bd46519bc904fe5bae9c1997ab99c32557934f80
Zeri-Time <blee.940604@gmail.com>
2025-05-31 21:50:31 +0900

    조건문 if, switch

---
+++ WarmUp/src/Practice_04.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Practice_04 {
+    public static void main (String[] args) {
+        int x = 1;
+        int y = 2;
+        boolean result = x > y;
+        System.out.println(result);
+        boolean result2 = (x + y) > 2;
+        System.out.println(result2);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("a 와 b의 값을 입력하세요");
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int sum = a + b;
+//        boolean compare = sum > 10;
+//        System.out.println("compareResult: " + compare);
+
+        int h = ++x;
+        System.out.println(h);
+        boolean result3 = true;
+        boolean result4 = !result3;
+        System.out.println(result3);
+        System.out.println(result4);
+
+        boolean ab = (x > 2) ? true : false;
+        System.out.println(ab);
+        String bc = (x > 2) ? "맞아요" : "아니예요";
+        System.out.println(bc);
+    }
+}
---
+++ WarmUp/src/Practice_05_condition.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Practice_05_condition {
+    public static void main (String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("정수를 입력해 주세요");
+//        int i = scanner.nextInt();
+//        scanner.close();
+//        if (i > 10) {
+//            System.out.println("10보다 큽니다");
+//        } else if (i == 10) {
+//            System.out.println("10과 같습니다");
+//        }
+//        else {
+//            System.out.println("10보다 작습니다");
+//        }
+
+        int num = (int) (Math.random() * 6) + 1;
+        //0.0보다 크거나 같고 6.0보다 작은 범위이기 때문에 +1을 하고 소수는 날려서 정수로 맞춤
+//        System.out.println(num);
+//        System.out.println(num + "번이 나왔습니다");
+        if (num ==1) {
+            System.out.println("1이 나왔습니다");
+        }else if (num==2) {
+            System.out.println("2가 나왔습니다");
+        }else if (num==3) {
+            System.out.println("3이 나왔습니다");
+        }else if (num==4) {
+            System.out.println("4가 나왔습니다");
+        }else if (num==5) {
+            System.out.println("5가 나왔습니다");
+        }else {
+            System.out.println("6이 나왔습니다");
+        }
+        
+        char alpha = 'a';
+        switch (alpha) {
+            case 'a':
+                System.out.println("a입니다");
+                break;
+            case 'b':
+                System.out.println("b입니다");
+                break;
+            default:
+                System.out.println("a나 b가 아닙니다");
+        }
+    }
+}

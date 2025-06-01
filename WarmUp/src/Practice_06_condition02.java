6ed43772c886fc8e522a6e31d4b02d051882ba02
Zeri-Time <blee.940604@gmail.com>
2025-06-01 12:54:51 +0900

    조건문 for

---
+++ WarmUp/src/Practice_06_condition02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Practice_06_condition02 {
+    public static void main (String[] args){
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+//            System.out.println(i);
+            sum += i;
+//            System.out.println(sum);
+        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("put value of a");
+//        int a = scanner.nextInt();
+//        for (; a <= 10; a++){
+//            int sum2 = a;
+//            sum2 = sum2 + a;
+//            System.out.println(a);
+//            System.out.println(sum2);
+//        scanner.close();
+//        }
+
+    }
+}

57dbad5c75b7ff255558e1ed9ade4ff3fb96ef38
Zeri-Time <blee.940604@gmail.com>
2025-06-03 19:06:48 +0900

    조건문 while break, continue

--- WarmUp/src/Practice_07_condition_while.java
+++ WarmUp/src/Practice_07_condition_while.java
@@ -5,11 +5,26 @@ public class Practice_07_condition_while {
 //            System.out.println(a);
 //            a++;
 //        }
-        int sum = 0;
-        while (a <= 10) {
-            sum += a;
-            a++;
+//        int sum = 0;
+//        while (a <= 10) {
+//            sum += a;
+//            a++;
+//        }
+//        System.out.println(sum);
+
+//        while (true) {
+//            int random = (int) (Math.random() * 6)+ 1;
+//            System.out.println(random);
+//            if (random == 6) {
+//                break;
+//            }
+//        }
+
+        for (int i = 0; i <=10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
         }
-        System.out.println(sum);
     }
 }

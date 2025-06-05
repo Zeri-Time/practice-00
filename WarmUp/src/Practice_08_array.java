5ae0557398d88b01c61d4fa9510d6490be5a0623
Zeri-Time <blee.940604@gmail.com>
2025-06-05 13:35:54 +0900

    배열

--- WarmUp/src/Practice_08_array.java
+++ WarmUp/src/Practice_08_array.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
 public class Practice_08_array {
     public static void main (String[] args){
         //배열은 같은 타입의 데이터만 저장가능 (예: int는 int만, String은 문자열끼리)
         // 한번 생성된 배열은 길이를 늘리거나 줄일수 없음
-        int[] intArray = {1, 2, 3};
-        int sum = 0;
-            for (int i = 0; i <=2; i++) {
-                sum += intArray[i];
+//        int[] intArray = {1, 2, 3};
+//        int sum = 0;
+//            for (int i = 0; i <=2; i++) {
+//                sum += intArray[i];
+//
+//            }
+//            double avg = (double) sum / 3;
+//            System.out.println("총합: " + sum);
+//            System.out.println("평균: " + avg);
+//
+//            int[] intArray = new int[3];
+//            System.out.println(intArray[0]);
+//            String[] stringArray = new String[3];
+//            System.out.println(stringArray[0]);
+//            boolean[] booleanArray = new boolean[3];
+//            System.out.println(booleanArray[0]);
+//            intArray[0] = 30;
+//            intArray[1] = 40;
+//            intArray[2] = 50;
+//            System.out.println(intArray[0]);
+//            for (int i = 0; i <3; i++){
+//                System.out.println(intArray[i]);
+//            }
+//            int num = intArray.length;
+//            System.out.println(num);
 
-            }
-            double avg = (double) sum / 3;
-            System.out.println("총합: " + sum);
-            System.out.println("평균: " + avg);
+            Scanner scanner = new Scanner (System.in);
+            int[] x = new int[3];
     }
 }
